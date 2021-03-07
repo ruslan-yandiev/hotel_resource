97e5ee6dcee9b5ea9dc4fb73ccea8c0298eeefd6
ruslan <sqwer1986@gmail.com>
2021-03-07 23:54:42 +0300

    create button block and create header block

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,7 +3,8 @@ import * as $ from 'jquery';
 function importAll(resolve) {
     resolve.keys().forEach(resolve);
 }
-
+// TODO Напоминалка! нет смысла подключать все scss and JS файлы в один так как мы и так их пропарсим и соберем
+// TODO не касается области видимости переменных как в js так и в scss (импортировать для удаленной работы с перменными)
 importAll(require.context('@/', true, /\.js$|\.scss$/));
 
 import json from '@/assets/myjson.json';
