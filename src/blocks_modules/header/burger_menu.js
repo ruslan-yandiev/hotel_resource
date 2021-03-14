218a3eca3a848276169e1ff8539a9b2473fb4fa0
ruslan <sqwer1986@gmail.com>
2021-03-14 22:51:12 +0300

    blocking scrolling of the main content when the burger menu is open

--- src/blocks_modules/header/burger_menu.js
+++ src/blocks_modules/header/burger_menu.js
@@ -3,5 +3,5 @@ const iconMenu = document.querySelector('.icon-menu');
 iconMenu.addEventListener('click', () => {
     iconMenu.classList.toggle('_active');
     document.querySelector('.menu__body').classList.toggle('_active');
-    // document.querySelector('body').classList.toggle('_lock');
+    document.querySelector('body').classList.toggle('_lock');
 });
