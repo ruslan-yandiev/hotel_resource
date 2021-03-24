d4bd911ce90aeb9908037f563b87164547f04f9a
ruslan <sqwer1986@gmail.com>
2021-03-24 17:35:36 +0300

    change accordion JS code

--- src/blocks_modules/accordion-menu/accordion-menu.js
+++ src/blocks_modules/accordion-menu/accordion-menu.js
@@ -1,53 +1,60 @@
-import * as $ from 'jquery';
-// TODO переписать ве на чистый JS
+// import * as $ from 'jquery';
 // TODO реализовать выпадание меню при нажатии на стрелку
 // TODO Оптимизировать навешивание обработчика событий на стрелки
 // TODO Закрытие аккордиона при нажатии в любое место
-$(function () {
-    var Accordion = function (el, multiple) {
-        this.el = el || {};
-        // more then one submenu open?
-        this.multiple = multiple || false;
+// $(function () {
+//     var Accordion = function (el, multiple) {
+//         this.el = el || {};
+//         // more then one submenu open?
+//         this.multiple = multiple || false;
 
-        var dropdownlink = this.el.find('.dropdownlink');
-        dropdownlink.on('click', { el: this.el, multiple: this.multiple }, this.dropdown);
-    };
+//         var dropdownlink = this.el.find('.dropdownlink');
+//         dropdownlink.on('click', { el: this.el, multiple: this.multiple }, this.dropdown);
+//     };
 
-    Accordion.prototype.dropdown = function (e) {
-        var $el = e.data.el,
-            $this = $(this),
-            //this is the ul.submenuItems
-            $next = $this.next();
+//     Accordion.prototype.dropdown = function (e) {
+//         var $el = e.data.el,
+//             $this = $(this),
+//             //this is the ul.submenuItems
+//             $next = $this.next();
+//         $next.slideToggle();
 
-        $next.slideToggle();
-        $this.parent().toggleClass('open');
+//         if (!e.data.multiple) {
+//             //show only one menu at the same time
+//             $el.find('.submenuItems').not($next).slideUp();
+//         }
+//     };
 
-        if (!e.data.multiple) {
-            //show only one menu at the same time
-            $el.find('.submenuItems').not($next).slideUp().parent().removeClass('open');
-        }
-    };
+//     var accordion = new Accordion($('.accordion-menu'), false);
+// });
 
-    var accordion = new Accordion($('.accordion-menu'), false);
-});
+let nontarget, nontarget2;
+let dropdownlink = document.querySelectorAll('.dropdownlink');
 
-let nontarget;
-
-document.querySelectorAll('.dropdownlink').forEach((elem) => {
+dropdownlink.forEach((elem) => {
     elem.addEventListener('click', (event) => {
-        let list = event.target.previousSibling.classList;
+        let arrow = event.target.previousSibling;
+        let submenuItems = event.target.nextSibling;
+        // submenuItems.style.display = 'block';
 
         if (nontarget) {
-            if (nontarget === list) {
-                list.toggle('_reverse_arrow');
+            if (nontarget === arrow) {
+                arrow.classList.toggle('_reverse_arrow');
+                submenuItems.classList.toggle('visibility');
             } else {
-                if (nontarget.contains('_reverse_arrow')) nontarget.toggle('_reverse_arrow');
-                list.toggle('_reverse_arrow');
+                if (nontarget.classList.contains('_reverse_arrow')) {
+                    nontarget.classList.toggle('_reverse_arrow');
+                    nontarget2.classList.toggle('visibility');
+                }
+                arrow.classList.toggle('_reverse_arrow');
+                submenuItems.classList.toggle('visibility');
             }
         } else {
-            list.toggle('_reverse_arrow');
+            arrow.classList.toggle('_reverse_arrow');
+            submenuItems.classList.toggle('visibility');
         }
 
-        nontarget = list;
+        nontarget = arrow;
+        nontarget2 = submenuItems;
     });
 });
